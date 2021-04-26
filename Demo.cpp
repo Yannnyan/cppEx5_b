414749fca8b78b446187cad0880587b37b4c2636
Erel Segal-Halevi cron <erelsgl@gmail.com>
2021-04-26 22:54:50 +0300

    Add tree of strings

--- Demo.cpp
+++ Demo.cpp
@@ -16,7 +16,7 @@ using namespace ariel;
 
 int main() {
   BinaryTree<int> tree_of_ints;
-  tree_of_ints.add_root(1)     
+  tree_of_ints.add_root(1)
   .add_left(1, 9)      // Now 9 is the left child of 1
   .add_left(9, 4)      // Now 4 is the left child of 9
   .add_right(9, 5)     // Now 5 is the right child of 9
@@ -32,16 +32,36 @@ int main() {
   */
 
   for (auto it=tree_of_ints.begin_preorder(); it!=tree_of_ints.end_preorder(); ++it) {
-    cout << (*it) << " " >> endl;
+    cout << (*it) << " " << endl;
   }  // prints: 1 2 4 5 3
   for (auto it=tree_of_ints.begin_inorder(); it!=tree_of_ints.end_inorder(); ++it) {
-    cout << (*it) << " " >> endl;
+    cout << (*it) << " " << endl;
   }  // prints: 4 2 5 1 3 
   for (auto it=tree_of_ints.begin_postorder(); it!=tree_of_ints.end_postorder(); ++it) {
-    cout << (*it) << " " >> endl;
+    cout << (*it) << " " << endl;
+  }  // prints: 4 5 2 3 1
+
+
+  // The same should work with other types, e.g. with strings:
+  
+  BinaryTree<string> tree_of_strings;
+  tree_of_strings.add_root("1")     
+  .add_left("1", "9")      // Now 9 is the left child of 1
+  .add_left("9", "4")      // Now 4 is the left child of 9
+  .add_right("9", "5")     // Now 5 is the right child of 9
+  .add_right("1", "3")     // Now 3 is the right child of 1
+  .add_left("1", "2");     // Now 2 is the left child of 1, instead of 9 (the children of 9 remain in place)
+  cout << tree_of_strings << endl; 
+
+  for (auto it=tree_of_strings.begin_preorder(); it!=tree_of_strings.end_preorder(); ++it) {
+    cout << (*it) << " " << endl;
+  }  // prints: 1 2 4 5 3
+  for (auto it=tree_of_strings.begin_inorder(); it!=tree_of_strings.end_inorder(); ++it) {
+    cout << (*it) << " " << endl;
+  }  // prints: 4 2 5 1 3 
+  for (auto it=tree_of_strings.begin_postorder(); it!=tree_of_strings.end_postorder(); ++it) {
+    cout << (*it) << " " << endl;
   }  // prints: 4 5 2 3 1
 
   
-
-  return 0;
 }
