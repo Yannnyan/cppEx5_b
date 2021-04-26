5feeeff9dc0710ff3d10744123aed146bf4abcb5
Erel Segal-Halevi cron <erelsgl@gmail.com>
2021-04-26 22:02:40 +0300

    first upload

---
+++ Demo.cpp
@@ -0,0 +1,47 @@
+/**
+ * Demo file for the exercise on binary tree
+ *
+ * @author Erel Segal-Halevi
+ * @since 2021-04
+ */
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+using namespace std;
+
+#include "BinaryTree.hpp"
+using namespace ariel;
+
+int main() {
+  BinaryTree<int> tree_of_ints;
+  tree_of_ints.add_root(1)     
+  .add_left(1, 9)      // Now 9 is the left child of 1
+  .add_left(9, 4)      // Now 4 is the left child of 9
+  .add_right(9, 5)     // Now 5 is the right child of 9
+  .add_right(1, 3)     // Now 3 is the right child of 1
+  .add_left(1, 2);     // Now 2 is the left child of 1, instead of 9 (the children of 9 remain in place)
+
+  cout << tree_of_ints << endl;  /* Prints the tree in a reasonable format, e.g.:
+        1
+        |--------|
+        2        3
+        |---|
+        4   5
+  */
+
+  for (auto it=tree_of_ints.begin_preorder(); it!=tree_of_ints.end_preorder(); ++it) {
+    cout << (*it) << " " >> endl;
+  }  // prints: 1 2 4 5 3
+  for (auto it=tree_of_ints.begin_inorder(); it!=tree_of_ints.end_inorder(); ++it) {
+    cout << (*it) << " " >> endl;
+  }  // prints: 4 2 5 1 3 
+  for (auto it=tree_of_ints.begin_postorder(); it!=tree_of_ints.end_postorder(); ++it) {
+    cout << (*it) << " " >> endl;
+  }  // prints: 4 5 2 3 1
+
+  
+
+  return 0;
+}
---
+++ Makefile
@@ -0,0 +1,32 @@
+#!make -f
+
+CXX=clang++-9
+CXXVERSION=c++2a
+CXXFLAGS=-std=$(CXXVERSION) -Werror -Wsign-conversion
+VALGRIND_FLAGS=-v --leak-check=full --show-leak-kinds=all  --error-exitcode=99
+TIDY_FLAGS=-extra-arg=-std=$(CXXVERSION) -checks=bugprone-*,clang-analyzer-*,cppcoreguidelines-*,performance-*,portability-*,readability-*,-cppcoreguidelines-pro-bounds-pointer-arithmetic,-cppcoreguidelines-owning-memory --warnings-as-errors=-*
+
+SOURCES=Tree.hpp
+OBJECTS=
+
+run: demo
+	./$^
+
+demo: Demo.o $(OBJECTS)
+	$(CXX) $(CXXFLAGS) $^ -o demo
+
+test: TestCounter.o Test.o $(OBJECTS)
+	$(CXX) $(CXXFLAGS) $^ -o test
+
+tidy:
+	clang-tidy $(SOURCES) $(TIDY_FLAGS) --warnings-as-errors=-* --
+
+valgrind: demo test
+	valgrind --tool=memcheck $(VALGRIND_FLAGS) ./demo 2>&1 | { egrep "lost| at " || true; }
+	valgrind --tool=memcheck $(VALGRIND_FLAGS) ./test 2>&1 | { egrep "lost| at " || true; }
+
+%.o: %.cpp
+	$(CXX) $(CXXFLAGS) --compile $< -o $@
+
+clean:
+	rm -f *.o demo test
